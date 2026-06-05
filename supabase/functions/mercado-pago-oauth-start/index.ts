7fbf098801dcd887cd8219ba7d3b6a4c7430a22d
carollip-web <carol.lip@gmail.com>
2026-06-05 11:31:35 -0300

    fix: adicionar platform_id=mp e usar auth.mercadopago.com na URL de OAuth

--- supabase/functions/mercado-pago-oauth-start/index.ts
+++ supabase/functions/mercado-pago-oauth-start/index.ts
@@ -83,7 +83,7 @@ serve(async (req) => {
       )
     }
 
-    const authUrl = `https://auth.mercadopago.com.br/authorization?client_id=${MERCADO_PAGO_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(MERCADO_PAGO_REDIRECT_URI)}&state=${state}`
+    const authUrl = `https://auth.mercadopago.com/authorization?client_id=${MERCADO_PAGO_CLIENT_ID}&response_type=code&platform_id=mp&redirect_uri=${encodeURIComponent(MERCADO_PAGO_REDIRECT_URI)}&state=${state}`
 
     return new Response(JSON.stringify({ authUrl }), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
